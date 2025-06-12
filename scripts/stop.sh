4fb2dfa63c4af3702826415739c50a80ab3dc2dc
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-12 11:54:37 +0500

    fixed issues

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-echo "Stopping container..." >> /home/ubuntu/stop.log
-docker stop python-app >> /home/ubuntu/stop.log 2>&1 || true
-docker rm python-app >> /home/ubuntu/stop.log 2>&1 || true
-echo "Stopped container." >> /home/ubuntu/stop.log
+echo "No containers to stop. Skipping..." > /home/ubuntu/stop.log
+exit 0
