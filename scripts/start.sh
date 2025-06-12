8bc06a6c3cf1de24ca2af677b30b858c453578ab
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-12 14:53:49 +0500

    fixed issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -7,5 +7,5 @@ aws ecr get-login-password --region eu-west-1 | docker login --username AWS --pa
 docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
 
 # Run the container
-docker run -d --name python-app -p 80:80 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
+docker run -d --name python-app -p 80:5000 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
 
