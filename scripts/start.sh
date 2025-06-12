a1dcce4901133dc226f4f53005f95e8b1c5bdc45
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-12 12:16:34 +0500

    fixed issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 cd /home/ubuntu/python-app
 
-# Stop any old container with same name
-sudo docker stop python-app || true
-sudo docker rm python-app || true
-
 # Pull and run the latest image
 sudo docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
 
 sudo docker run -d \
-  --name python-app \
+  --name python \
   -p 80:5000 \
-  099199746132.dkr.ecr.<region>.amazonaws.com/demo-repo:latest
+  099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
