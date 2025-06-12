04a3aee2cc3fde86414905d8ac63bf8717354a4b
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-12 11:44:10 +0500

    fixed issues

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-sudo docker stop python-app || true
-sudo docker rm python-app || true
+echo "Stopping container..." >> /home/ubuntu/stop.log
+docker stop python-app >> /home/ubuntu/stop.log 2>&1 || true
+docker rm python-app >> /home/ubuntu/stop.log 2>&1 || true
+echo "Stopped container." >> /home/ubuntu/stop.log
