cb743580178b0d42e2b1a9d530fdfaa002e80d8e
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-12 15:00:53 +0500

    fixed issues

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ try:
 
     @app.route('/')
     def home():
-        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!!"
+        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!! from docker EC2"
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
