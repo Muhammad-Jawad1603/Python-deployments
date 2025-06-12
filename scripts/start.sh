c03ab8f24c9b23faf9f5813eb6b3f45ef4268ea3
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-12 12:28:57 +0500

    fixed issues

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cd /home/ubuntu/python-app
 
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 099199746132.dkr.ecr.eu-west-1.amazonaws.com
+
 # Pull and run the latest image
 sudo docker pull 099199746132.dkr.ecr.eu-west-1.amazonaws.com/demo-repo:latest
 
