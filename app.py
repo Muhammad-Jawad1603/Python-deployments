0ddf6101631416f6249220b64b892923313acbc8
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-23 17:22:22 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ try:
 
     @app.route('/')
     def home():
-        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!! ECS"
+        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!!"
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
