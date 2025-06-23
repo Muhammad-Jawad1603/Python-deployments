f4f7133389e2bb2234c2232a7f4ca0ca110706d1
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-23 16:17:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ try:
 
     @app.route('/')
     def home():
-        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!! from docker ECS!!!!!"
+        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!! ECS"
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
