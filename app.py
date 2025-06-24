378f1dd0b11a40f48016dc782a58304e73b45253
Muhammad-Jawad1603 <muhammadjawad1603@gmail.com>
2025-06-24 10:40:19 +0500

    Update app.py......

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ try:
 
     @app.route('/')
     def home():
-        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!! python"
+        return "Hello, world!!!!!!!!!!!!!!!!!!!!!!!!!! python!!!!!"
 
     if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
